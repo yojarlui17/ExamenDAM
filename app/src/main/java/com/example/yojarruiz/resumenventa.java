337bf0e0ee79f903ab7068e30d279d921f6e8ec9
yojarlui17 <yojarlui18@gmail.com>
2020-05-29 17:33:41 -0500

    desarrollo del diseño

---
+++ app/src/main/java/com/example/yojarruiz/resumenventa.java
@@ -0,0 +1,14 @@
+package com.example.yojarruiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resumenventa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumenventa);
+    }
+}
---
+++ app/src/main/java/com/example/yojarruiz/venderentrada.java
@@ -0,0 +1,14 @@
+package com.example.yojarruiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class venderentrada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venderentrada);
+    }
+}
